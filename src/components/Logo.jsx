13ba71b5639227737851fed4acfbbd4f2cb80c94
ZAS0 <zeeshan.ahmed.siddiqui07@gmail.com>
2025-11-03 15:27:22 +0530

    Added Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,9 @@
+import logo from "../assets/Logo2.png"
+
 function Logo({ width = "100px" }) {
-  return <div>Logo</div>;
+  return <div>
+    <img src={logo} alt="" style={{width}} className="rounded-full" />
+  </div>;
 }
 
 export default Logo;
